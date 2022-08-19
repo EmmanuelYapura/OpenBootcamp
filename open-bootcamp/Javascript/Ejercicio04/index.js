b655de503b23b51b8fc1692dfcf19cccd45384c6
Emmanuel Yapura <eyapura96@gmail.com>
2022-08-19 20:51:45 -0300

    agrega ejercicio 04

---
+++ open-bootcamp/Javascript/Ejercicio04/index.js
@@ -0,0 +1,12 @@
+let nombre = 'Emmanuel';
+let apellido = "Yapura";
+let estudiante = `${nombre} ${apellido}`;
+let estudianteMayus = estudiante.toUpperCase();
+let estudianteMinuss = estudiante.toLowerCase();
+let longitudEstudiante = estudiante.length;
+let primerLetraNombre = nombre[0];
+let ultimaLetraApellido = apellido[apellido.length - 1]
+let estudianteSinEspacios = estudiante.trim();
+let nombreEnEstudiante = estudiante.includes(nombre)
+
+
