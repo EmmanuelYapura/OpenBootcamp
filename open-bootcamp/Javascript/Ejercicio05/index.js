6f58b82d6d2c79d36c494e2ea8813d01c233fdeb
Emmanuel Yapura <eyapura96@gmail.com>
2022-08-22 21:36:02 -0300

    agrega ejercicio 04

---
+++ open-bootcamp/Javascript/Ejercicio05/index.js
@@ -0,0 +1,15 @@
+let alturaCms = 175;
+
+let alturaMts = 1.75;
+
+let peso = 69;
+
+let alturaMtsArriba = Math.ceil(1.75);
+
+let alturaMtsAbajo = Math.floor(1.75);
+
+let max_valor_JS = Number.MAX_VALUE; 
+
+let max_valor_JS_Mas_Uno = Number.MAX_VALUE + 1;
+
+console.log(max_valor_JS === max_valor_JS_Mas_Uno)
