4f1f5c9311649ecb79dcf56b0de0b37446048935
Emmanuel Yapura <eyapura96@gmail.com>
2022-08-23 17:35:44 -0300

    ejercicio 07 - fechas

---
+++ open-bootcamp/Javascript/Ejercicio07/fechas.js
@@ -0,0 +1,9 @@
+const fechaActual = new Date();
+
+const fechaNacimiento = new Date(1996, 10, 3);
+
+const compararFechas = fechaActual > fechaNacimiento;
+
+const mesNacimiento = fechaNacimiento.getMonth() + 1;
+
+const anioNacimiento = fechaNacimiento.getFullYear();
