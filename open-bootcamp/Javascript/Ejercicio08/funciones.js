ebf780950875390fa20fbfa1581bde6dc3f99085
Emmanuel Yapura <eyapura96@gmail.com>
2022-08-24 16:22:24 -0300

    agrega soluciones del bootcamp

--- open-bootcamp/Javascript/Ejercicio08/funciones.js
+++ open-bootcamp/Javascript/Ejercicio08/funciones.js
@@ -19,3 +19,16 @@ function* generadorIndicesPares() {
         yield id;
     }
 }
+
+//Soluciones del Bootcamp
+
+async function damePromesa() {
+    return setTimeout(() => console.log("Hola soy una promesa"), 5000)
+}
+
+function* idsPares() {
+    let id = 0
+    while(true) {
+        yield id += 2
+    }
+}
