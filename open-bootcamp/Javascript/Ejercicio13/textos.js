cb65dc90fc843f0cf4d617dbb248d0e2a14b4e0f
Emmanuel Yapura <eyapura96@gmail.com>
2022-08-29 20:12:02 -0300

    agrega ejercicio 13

---
+++ open-bootcamp/Javascript/Ejercicio13/textos.js
@@ -0,0 +1,18 @@
+
+let nombre = "Emmanuel";
+let apellido = "Yapura";
+let estudiante = `${nombre} ${apellido}`;
+
+let estudianteMayus = estudiante.toUpperCase();
+
+let estudianteMinuss = estudiante.toLowerCase();
+
+let longitudEstudiante = estudiante.length;
+
+let primerLetraNombre = nombre[0];
+
+let ultimaLetraApellido = apellido[apellido.length - 1];
+
+let estudianteSinEspacios = estudiante.replace(/ /g, "");
+
+let nombreEnEstudiante = estudiante.includes(nombre);
